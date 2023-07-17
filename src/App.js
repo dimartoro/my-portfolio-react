228e14aaa803194c145d87aa3ca602b28a66dd62
Diana Castro <dimartoro@gmail.com>
2023-07-17 18:05:20 -0400

    cleaning code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { useState } from 'react';
 
 // TODO: Add a comment explaining what this function is doing
 // This function is a functional component that helps us split the UI into distinct parts.
-// In this case, we are returning another component, <HelloReact/> from it.
 function App() {
 
   const[activeMenu,setMenu] = useState('');
